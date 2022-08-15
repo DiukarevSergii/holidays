621f993e368ae42aa330a29f8afdee122ba854d3
diukarev.sergii <diukarev.sergii@gmail.com>
2022-08-15 17:38:24 +0300

    add Data Access Layer

--- src/main/java/com/serhiidiukarev/holiday/repository/HolidayRepository.java
+++ src/main/java/com/serhiidiukarev/holiday/repository/HolidayRepository.java
@@ -2,8 +2,18 @@ package com.serhiidiukarev.holiday.repository;
 
 import com.serhiidiukarev.holiday.Holiday;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface HolidayRepository extends JpaRepository<Holiday, Long> {
+
+    @Query("SELECT holiday FROM Holiday holiday " +
+            "WHERE holiday.holidayDate = ?1 " +
+            "AND holiday.holidayName = ?2 " +
+            "AND holiday.holidayCategory = ?3") // todo reimplement to criteria
+    Optional<Holiday> findHoliday(LocalDate holidayDate, String holidayName, Holiday.HolidayCategory holidayCategory);
 }
