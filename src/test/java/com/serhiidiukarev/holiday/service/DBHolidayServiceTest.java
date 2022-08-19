2ccb6ab662c4028188491cc02cff94371434f9bf
diukarev.sergii <diukarev.sergii@gmail.com>
2022-08-19 12:11:56 +0300

    add new tests

---
+++ src/test/java/com/serhiidiukarev/holiday/service/DBHolidayServiceTest.java
@@ -0,0 +1,220 @@
+package com.serhiidiukarev.holiday.service;
+
+import com.serhiidiukarev.holiday.Holiday;
+import com.serhiidiukarev.holiday.repository.HolidayRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class DBHolidayServiceTest {
+
+    private HolidayService<LocalDate, String> underTest;
+    private Map<LocalDate, Set<Holiday>> holidays; //fixme remove later
+    @Mock
+    private HolidayRepository holidayRepository;
+
+    @BeforeEach
+    public void setUp() {
+        underTest = new DBHolidayService(holidayRepository);
+    }
+
+    @Test
+    public void testGetHolidays() {
+        //when
+        underTest.getHolidays();
+        //then
+        verify(holidayRepository).findAll();
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"2000-01-04", "2000-02-04", "2001-01-04"})
+    public void testAddHoliday(
+            @ConvertWith(LocalDateConverter.class) LocalDate date) {
+        underTest.addHoliday(date);
+
+        assertEquals(1, holidays.size());
+
+        Set<Holiday> holidaysSet = holidays.get(date);
+
+        assertNotNull(holidaysSet);
+        assertEquals(1, holidays.get(date).size());
+
+        Holiday holiday = holidaysSet.iterator().next();
+        assertNotNull(holiday);
+
+        assertEquals(date, holiday.getHolidayDate());
+        assertEquals(date.toString(), holiday.getHolidayName());
+        assertEquals(Holiday.HolidayCategory.CUSTOM, holiday.getHolidayCategory());
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"2000-01-04", "2000-02-04", "2001-01-04"})
+    public void addHoliday_OneDate_true(
+            @ConvertWith(LocalDateConverter.class) LocalDate date) {
+        assertTrue(underTest.addHoliday(date));
+        assertEquals(1, holidays.size());
+
+        Set<Holiday> holidaysSet = holidays.get(date);
+
+        assertNotNull(holidaysSet);
+        assertEquals(1, holidaysSet.size());
+
+        Holiday holiday = holidaysSet.iterator().next();
+        assertNotNull(holiday);
+
+        assertEquals(date, holiday.getHolidayDate());
+        assertEquals(date.toString(), holiday.getHolidayName());
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"2000-01-04", "2000-02-04", "2001-01-04"})
+    public void addHoliday_Twice_false(
+            @ConvertWith(LocalDateConverter.class) LocalDate date) {
+        underTest.addHoliday(date);
+        assertFalse(underTest.addHoliday(date));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"2000-01-01,2000-01-03", "2000-02-01,2000-02-03", "2001-01-01,2001-01-03"})
+    public void addHolidaysBetween_ThreeDays_Size3(
+            @ConvertWith(LocalDateConverter.class) LocalDate startDate,
+            @ConvertWith(LocalDateConverter.class) LocalDate endDate) {
+        underTest.addHolidaysBetween(startDate, endDate);
+        assertEquals(3, underTest.getHolidays().size());
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"2000-01-01", "2022-07-16", "2022-07-23"})
+    public void countWorkingDaysBetween_SATURDAY_0(
+            @ConvertWith(LocalDateConverter.class) LocalDate date) {
+        assertEquals(DayOfWeek.SATURDAY, date.getDayOfWeek());
+        assertEquals(0, underTest.countWorkingDaysBetween(date, date));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"2000-01-02", "2022-07-17", "2022-07-24"})
+    public void countWorkingDaysBetween_SUNDAY_0(
+            @ConvertWith(LocalDateConverter.class) LocalDate date) {
+        assertEquals(DayOfWeek.SUNDAY, date.getDayOfWeek());
+        assertEquals(0, underTest.countWorkingDaysBetween(date, date));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"2022-06-27,2022-07-04,2022-07-01", "2022-07-29,2022-08-05,2022-08-02"})
+    public void countWorkingDaysBetween_TwoDatesWithAdditionalHoliday_5(
+            @ConvertWith(LocalDateConverter.class) LocalDate startDate,
+            @ConvertWith(LocalDateConverter.class) LocalDate endDate,
+            @ConvertWith(LocalDateConverter.class) LocalDate holidayDate
+    ) {
+        underTest.addHoliday(holidayDate);
+        assertEquals(5, underTest.countWorkingDaysBetween(startDate, endDate));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"src/test/resources/data01.json,2", "src/test/resources/data02.json,1096"})
+    public void addHolidaysFromJSON_PathToJson_AddObjectsToHolidays(String jsonDestination, int expectedSize) {
+        underTest.addHolidaysFromJSON(jsonDestination);
+
+        assertEquals(expectedSize, holidays.size());
+
+        Set<Holiday> holidaySet = holidays.get(LocalDate.of(2020, 1, 1));
+        assertEquals(2, holidaySet.size());
+
+        assertEquals(1, holidays.get(LocalDate.of(2020, 1, 4)).size());
+
+        holidays.values().forEach(map -> {
+            assertNotNull(map);
+            map.forEach(holiday -> {
+                assertNotNull(holiday);
+                assertNotNull(holiday.getHolidayId());
+                assertNotNull(holiday.getHolidayName());
+                assertNotNull(holiday.getHolidayName());
+                assertNotNull(holiday.getHolidayCategory());
+            });
+        });
+
+        Holiday custom0Test = Holiday
+                .builder()
+                .holidayDate(LocalDate.of(2020, 1, 1))
+                .holidayCategory(Holiday.HolidayCategory.CUSTOM)
+                .holidayName("CUSTOM0")
+                .build();
+
+        Holiday custom0Version2Test = Holiday
+                .builder()
+                .holidayDate(LocalDate.of(2020, 1, 1))
+                .holidayCategory(Holiday.HolidayCategory.CUSTOM)
+                .holidayName("CUSTOM0 - version 2")
+                .build();
+
+        Set<Holiday> customSet = Set.of(custom0Test, custom0Version2Test);
+
+        assertEquals(customSet, holidaySet);
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({"src/test/resources/file-to-write.json,src/test/resources/file-to-read.json"})
+    public void writeHolidaysToJSON_PathToJson_AddAllObjectsToHolidays(String toWrite, String toRead) throws IOException {
+        new File(toWrite).delete();
+
+        Holiday holiday0 = Holiday
+                .builder()
+                .holidayDate(LocalDate.of(2020, 1, 1))
+                .holidayCategory(Holiday.HolidayCategory.CUSTOM)
+                .holidayName("CUSTOM")
+                .build();
+
+        Holiday holiday1 = Holiday
+                .builder()
+                .holidayDate(LocalDate.of(2021, 1, 1))
+                .holidayCategory(Holiday.HolidayCategory.OTHER)
+                .holidayName("OTHER")
+                .build();
+
+        Holiday holiday2 = Holiday
+                .builder()
+                .holidayDate(LocalDate.of(2020, 1, 1))
+                .holidayCategory(Holiday.HolidayCategory.GOVERNMENT)
+                .holidayName("GOVERNMENT")
+                .build();
+
+        underTest.addHoliday(holiday0);
+        underTest.addHoliday(holiday1);
+        underTest.addHoliday(holiday2);
+        underTest.writeHolidaysToJSON(toWrite);
+
+        String actual = Files.readString(Path.of(toWrite));
+        String expected = Files.readString(Path.of(toRead));
+
+        assertEquals(expected, actual);
+    }
+
+}
